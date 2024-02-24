5e0540c982d41dadf2c40e4b55c24b433d384bd7
Zainul <zainulbasharnazmin@gmail.com>
2024-02-24 14:03:09 +0530

    commit2

--- src/MainPage/Product/PurchaseOrder1.js
+++ src/MainPage/Product/PurchaseOrder1.js
@@ -1,37 +1,25 @@
-import React from 'react';
-import {useState} from 'react'
+import React, { useState } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-import './productList.css'
+import './productList.css';
 import ProductTable from './ProductTable';
 import 'react-date-range/dist/styles.css'; // main css file
-import 'react-date-range/dist/theme/default.css'; 
+import 'react-date-range/dist/theme/default.css';
 import { DateRangePicker } from 'react-date-range';
 import { addDays } from 'date-fns';
-
-// import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-// import { LocalizationProvider } from '@mui/x-date-pickers-pro';
-// import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
-// import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
-
-  
+import { enGB } from 'date-fns/locale'; 
 
 const PurchaseOrder1 = () => {
+  const [datePicker, setDatepicker] = useState(false);
+  const [daily, setDaily] = useState(false);
 
- const [datePicker, setDatepicker] = useState(false);
- const [daily, setDaily] = useState(false);
-//  const [weekly, setWeekly] = useState(false);
-//  const [monthly, setMonthly] = useState(false);
+  const dailyFnc = () => {
+    setDaily(!daily);
+  };
 
-
- const dailyFnc = () => {
-        setDaily(!daily)
- }
-
- const datePickerFnc = () => {
-    
-  setDatepicker(!datePicker);
- }
+  const datePickerFnc = () => {
+    setDatepicker(!datePicker);
+  };
 
   const [state, setState] = useState([
     {
@@ -39,67 +27,70 @@ const PurchaseOrder1 = () => {
       endDate: addDays(new Date(), 7),
       key: 'selection'
     }
-    
   ]);
-  
 
-    return (
-        <>
-         <div className='wholeComp'>
-            
-            <div className='Porder'>
-            <h3>Purchase order report</h3>
-            <h5>Manage your Purchase order report</h5>
+  return (
+    <>
+      <div className="wholeComp">
+        <div className="Porder">
+          <h3>Purchase order report</h3>
+          <h5>Manage your Purchase order report</h5>
+        </div>
+        <div className="twoDropdown">
+          <DropdownButton title="Summary" className="drop5">
+            <Dropdown.Item href="#/action-1">Daily</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Weekly</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Monthly</Dropdown.Item>
+          </DropdownButton>
+
+          <DropdownButton id="dropdown-basic-button" title="FilterBy">
+            <Dropdown.Item href="#/action-1" onClick={dailyFnc}>
+              Load Number
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-2" onClick={dailyFnc}>
+              Category
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-3" onClick={dailyFnc}>
+              Brand
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-3" onClick={dailyFnc}>
+              sku
+            </Dropdown.Item>
+          </DropdownButton>
+          <div className="inputFilterBY">
+            {daily && <input className="inputF" placeholder="enter" />}
+          </div>
+
+          <div className="daterangepicker d-flex" onClick={datePickerFnc}>
+            <div className="m-1 px-2 py-1 bg-light">
+              <p>02-11-24</p>
             </div>
-    <div className='twoDropdown'> 
-      <DropdownButton  title="Summary" className='drop5'>
-        <Dropdown.Item href="#/action-1">Daily</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Weekly</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Monthly</Dropdown.Item>
-      </DropdownButton>
-   
-    <DropdownButton id="dropdown-basic-button" title="FilterBy">
-      <Dropdown.Item href="#/action-1" onClick={dailyFnc}>Load Number</Dropdown.Item>
-      <Dropdown.Item href="#/action-2" onClick={dailyFnc}>Category</Dropdown.Item>
-      <Dropdown.Item href="#/action-3" onClick={dailyFnc}>Brand</Dropdown.Item>
-      <Dropdown.Item href="#/action-3" onClick={dailyFnc}>sku</Dropdown.Item>
-    </DropdownButton>
-    <div className='inputFilterBY'>
-      {
-        daily &&
-        <input className='inputF' placeholder='enter'></input>
-      }
-    </div>
+            <div className="m-1 px-2 py-1 bg-light">
+              <p>03-12-24</p>
+            </div>
+          </div>
 
-<div className=' daterangepicker d-flex' onClick={datePickerFnc}>
-    
-  <div className='m-1 px-2 py-1 bg-light'>
-    <p>02-11-24</p>
-  </div>
-  <div className='m-1 px-2 py-1 bg-light' >
-    <p>03-12-24</p>
-  </div>
-</div>
- 
-    { datePicker && <DateRangePicker
-  onChange={item => setState([item.selection])}
-  showSelectionPreview={true}
-  moveRangeOnFirstSelection={false}
-  months={1}
-  ranges={state}
-  direction="horizontal"
-  preventSnapRefocus={true}
-  calendarFocus="backwards"
-/>}
-</div>
+          {datePicker && (
+            <DateRangePicker
+              onChange={(item) => setState([item.selection])}
+              showSelectionPreview={true}
+              moveRangeOnFirstSelection={false}
+              months={1}
+              ranges={state}
+              direction="horizontal"
+              preventSnapRefocus={true}
+              calendarFocus="backwards"
+              locale={enGB} // Set the locale here
+            />
+          )}
+        </div>
 
-     <h4 className='Pdate'>02-24-24</h4>
+        <h4 className="Pdate">02-24-24</h4>
 
-       <ProductTable/>
-     </div>
-   
-        </>
-    )
-}
+        <ProductTable />
+      </div>
+    </>
+  );
+};
 
 export default PurchaseOrder1;
